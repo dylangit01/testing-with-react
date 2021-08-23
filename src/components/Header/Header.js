2f4fae82c98daa098bf4de3002d44fa97db94556
Dylan <dcren2010@gmail.com>
2021-08-22 23:22:42 -0400

    adding styles to Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './styles.scss';
+import Logo from '../../assets/graphics/logo-dd.png';
+
 
 const Header = () => {
 	return (
-		<div>
-			Header
-		</div>
+		<header>
+			<div className='wrapper'>
+				<div className='logo'>
+					<img src={Logo} alt="logo" />
+				</div>
+			</div>
+		</header>
 	)
 }
 
